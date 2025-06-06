fc57ba8edbcb03134e19ea52e1eeccf509a51340
0-don <don.cryptus@gmail.com>
2025-06-06 22:47:45 +0200

    Refactor MyComponent and improve DOM injection logic in main function

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use this for React 18+
+import ReactDOM from "react-dom";
 
 // 1. Define your React component
 function MyComponent() {
   return (
-    <div style={{ color: "white", margin: "1rem" }}>My Test Component!</div>
+    <div style={{ color: "white", margin: "1rem" }}>
+      My Test Component for Traasdasdhbin+!
+    </div>
   );
 }
 
 async function main() {
-  // Wait for Spicetify and the DOM to be ready
-  while (
-    !Spicetify?.showNotification ||
-    !document.querySelector(".main-view-container__scroll-node")
-  ) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  }
-
-  console.log("Spicetify is ready! Attaching React component.");
-  Spicetify.showNotification("Hello from Trashbin+!");
+  console.log("Spicetify is ready! Attaching Trashbin+ React component.");
+  Spicetify.showNotification("Hello from Trashbin+!!!!");
 
   // 2. Create a div to mount our component into
   const appRoot = document.createElement("div");
   appRoot.id = "trashbin-plus-root";
 
   // 3. Find a place in the DOM to inject our div
-  // The 'main-view-container' is a good place to inject content on most pages.
-  // We will prepend it to make sure it's at the top.
   const mainView = document.querySelector(".main-view-container__scroll-node");
 
   if (!mainView) {
@@ -37,12 +29,12 @@ async function main() {
   mainView.prepend(appRoot);
 
   // 4. Use ReactDOM to render our component into our div
-  // For React 18+ (which you are using based on your package.json)
-  const root = ReactDOM.createRoot(appRoot);
+  // This will work because Spicetify.ReactDOM has the createRoot method.
+  const root = (ReactDOM as any).createRoot(appRoot);
   root.render(<MyComponent />);
 
-  // It's good practice to provide a cleanup function for your extension
-  // This will be called when the extension is disabled or unloaded
+  // It's good practice to provide a cleanup function for your extension.
+  // This will be called when the extension is disabled or unloaded.
   return () => {
     console.log("Unmounting Trashbin+ component.");
     root.unmount();
